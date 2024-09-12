cc836c10523e83a888c9df630b60266347480487
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-12 19:46:47 +0200

    ReportController update: include Invoice model

--- Controllers/ReportController.php
+++ Controllers/ReportController.php
@@ -1,14 +1,26 @@
 <?php
 require_once __DIR__ . '/../models/Payment.php';
+require_once __DIR__ . '/../models/Invoice.php';
 
 class ReportController
 {
   private $payment;
+  private $invoice;
 
   public function __construct($db){
     $this->payment = new Payment($db);
+    $this->invoice = new Invoice($db);
   }
+
   public function showOverPayments(){
     return $this->payment->getOverpayments();
+  }
+
+  public function showUnderPayments(){
+    return $this->payment->getUnderpayments();
+  }
+
+  public function showUnselttledInvoices(){
+    return $this->invoice->getUnsettledInvoices();
   }
 }
