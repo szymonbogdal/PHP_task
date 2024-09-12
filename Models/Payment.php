b4802ae81937816641b4d3b5749095318b3d80f3
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-12 19:42:34 +0200

    Payment model update: method to get all underpayments

--- Models/Payment.php
+++ Models/Payment.php
@@ -30,4 +30,28 @@ class Payment
 
     return $overpayments;
   }
+
+  public function getUnderpayments(){
+    $query = 
+      "SELECT 
+        c.name AS customer_name,
+        i.number AS invoice_number,
+        i.total_amount AS invoice_total,
+        SUM(p.amount) AS total_paid,
+        (i.total_amount - SUM(p.amount)) AS underpayment_amount
+      FROM payments p
+      JOIN invoices i ON p.invoice_id = i.id
+      JOIN customers c ON i.customer_id = c.id
+      GROUP BY i.id
+      HAVING underpayment_amount > 0
+      ORDER BY underpayment_amount DESC";
+
+    $result = $this->db->query($query);
+    $underpayments = [];
+    while ($row = $result->fetch_assoc()){
+        $underpayments[] = $row;
+    }
+
+    return $underpayments;
+  }
 }
