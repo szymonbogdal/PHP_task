631a801a9f710a4cbec6ff5c2285fb3be3b974a0
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-13 15:46:11 +0200

    Add css styling for full page

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ if($action){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Public/css/style.css">
   <title>Reports</title>
 </head>
 <body>
