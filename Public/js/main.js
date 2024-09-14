05554a382a33075d0430c88fde92c2814d649718
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-14 17:44:43 +0200

    main.js update: add input event listener to call api with search parameters

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,20 +1,46 @@
 import { getData } from "./api.js";
 import { updateTable } from "./report.js";
+import { debounce } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", async ()=>{
-  const initialData = await getData("overpayments");
-  updateTable(initialData, "overpayments");
+  let currentAction = "overpayments";
+  let searchParams = {};
+
+  const initialData = await getData(currentAction, searchParams);
+  updateTable(initialData, currentAction);
+  
   const buttons = document.querySelectorAll('button[data-action]');
+  const filters = document.querySelectorAll('input[data-column]');
+
   buttons.forEach(button=>{
     button.addEventListener('click', async ()=>{
+      filters.forEach(input => input.value = "");
+      searchParams = {};
+      
       buttons.forEach(btn => btn.classList.remove('nav-active'));
       button.classList.add('nav-active');
       
-      const action = button.dataset.action;
-      const data = await getData(action);
-      if (data) {
-        updateTable(data, action);
+      currentAction = button.dataset.action;
+      const data = await getData(currentAction, searchParams);
+      if(data){
+        updateTable(data, currentAction);
       }
+    })
+  })
+
+  const debouncedSearch = debounce(async (column, value)=>{
+    searchParams[column] = value;
+    const data = await getData(currentAction, searchParams);
+    if(data){
+      updateTable(data, currentAction);
+    }
+  }, 300);
+
+  filters.forEach(filter=>{
+    filter.addEventListener('input', (e)=>{    
+      const column = e.target.dataset.column;
+      const value = e.target.value;
+      debouncedSearch(column, value);
     })
   })
 })
