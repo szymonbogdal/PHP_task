ee2123f12277c013669e99748a543b41b278b243
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-14 23:42:33 +0200

    report.js update: dispaly arrows next to table header

--- Public/js/report.js
+++ Public/js/report.js
@@ -40,22 +40,22 @@ function updateTableHeaders(action){
   const tableHead = document.querySelector('table thead tr');
 
   tableHead.innerHTML = `
-    <th data-sort="customer_name">Client name</th>
-    <th data-sort="invoice_number">Invoice number</th>
-    <th data-sort="issue_date">Issue date</th>
-    <th data-sort="due_date">Due date</th>
-    <th data-sort="invoice_total">Full amount</th>
+    <th data-sort="customer_name">Client name &#8597;</th>
+    <th data-sort="invoice_number">Invoice number &#8597;</th>
+    <th data-sort="issue_date">Issue date &#8597;</th>
+    <th data-sort="due_date">Due date &#8597;</th>
+    <th data-sort="invoice_total">Full amount &#8597;</th>
   `;
 
   if(action === 'overpayments'){
     tableHead.innerHTML += `
-      <th data-sort="overpayment_amount">Overpayment amount</th>
-      <th data-sort="total_paid">Total paid</th>
+      <th data-sort="overpayment_amount">Overpayment amount &#8597;</th>
+      <th data-sort="total_paid">Total paid &#8597;</th>
     `;
   }else if (action === 'underpayments'){
     tableHead.innerHTML += `
-      <th data-sort="underpayment_amount">Underpayment amount</th>
-      <th data-sort="total_paid">Total paid</th>
+      <th data-sort="underpayment_amount">Underpayment amount &#8597;</th>
+      <th data-sort="total_paid">Total paid &#8597;</th>
     `;
   }
 }
