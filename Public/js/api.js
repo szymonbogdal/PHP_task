9cf4237db85e666ddd32de0e604c0543534dc7de
Szymon Bogdał <bogdal.symon@gmail.com>
2024-09-14 21:52:49 +0200

    api.js update: getData method implement sort parameters into url

--- Public/js/api.js
+++ Public/js/api.js
@@ -1,10 +1,11 @@
-export async function getData(action, searchParams){
+export async function getData(action, searchParams, sortParams){
   let url = `index.php?action=${action}`;
 
   const searchEntries = Object.entries(searchParams);
   if(searchEntries.length > 0){
     url += '&' + searchEntries.map(([key, value]) => `${key}=${encodeURIComponent(value)}`).join('&');
   }
+  url += `&sort_by=${encodeURIComponent(sortParams.sort_by)}&order_by=${encodeURIComponent(sortParams.order_by)}`;
 
   try{
     const resposne = await fetch(url);
